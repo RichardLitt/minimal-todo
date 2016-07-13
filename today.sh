435fa2a85501b604d16268ea6c1be3ef32e0a12c
Richard Littauer <richard.littauer@gmail.com>
2016-07-13 10:06:13 +0100

    Update with BASEDIR env var

--- today.sh
+++ today.sh
@@ -1,10 +1,15 @@
 #!bin/sh
+# Set BASEDIR in your .bash_profile
 
-FILE="log/$(date +%Y-%m-%d).md"
-echo "# $(date +%Y-%m-%d)\n" >> $FILE
-cat "daily_routines.md" >> $FILE
-echo "\n## To Do\n" >> $FILE
-[ -f "log/$(date -v-1d +%Y-%m-%d).md" ] &&  sed '1,/## Tomorrow/d' "log/$(date -v-1d +%Y-%m-%d).md"| tail -n+2 >> $FILE || echo "Note: Yesterday not copied."
-awk '/# To Do/ {f=1} /----------/ {f=0} f' todo.md | tail -n+3 >> $FILE
-echo "Todos written, opening file..."
-open -a "Sublime Text" $FILE
+TODAY=$BASEDIR"log/$(date +%Y-%m-%d).md"
+
+touch $TODAY
+echo "# $(date +%Y-%m-%d)\n" >> $TODAY
+cat $BASEDIR"daily_routines.md" >> $TODAY
+echo "\n## To Do\n" >> $TODAY
+[ -f $BASEDIR"log/$(date -v-1d +%Y-%m-%d).md" ] &&  sed '1,/## Tomorrow/d' $BASEDIR"log/$(date -v-1d +%Y-%m-%d).md"| tail -n+2 >> $TODAY || echo "Note: Yesterday not copied."
+awk '/# To Do/ {f=1} /----------/ {f=0} f' $BASEDIR"todo.md" | tail -n+3 >> $TODAY
+echo "\n## Tomorrow\n" >> $TODAY
+echo "TODOs written"
+echo "Opening file..."
+open -a "Sublime Text" $BASEDIR"log/$(date +%Y-%m-%d).md"
